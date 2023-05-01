0fc765f6642b41680578e35d68d2c619a3071ae3
lejames <okejames55@gmail.com>
2023-05-01 14:19:11 +0300

    Initialize tomtom vector map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
+import tt from "@tomtom-international/web-sdk-maps"
 
 function App() {
+  const [ map, setMap ] = useState({})
+  const mapElement = useRef()
+  const longitude = 36.8218
+  const latitude = -1.2921
+
+  useEffect(() => {
+    let map = tt.map({
+      key: process.env.REACT_APP_TOM_TOM_API_KEY,
+      container: mapElement.current,
+      center: [longitude, latitude],
+      zoom: 14
+    })
+
+    setMap(map)
+
+  }, [])
+
   return (
     <div className="App">
-      
+      <div ref={mapElement} className="map"></div>
     </div>
   );
 }
