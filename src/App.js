7b784816db5be28f676a560105214ebf9d2bfc99
lejames <okejames55@gmail.com>
2023-05-14 13:11:22 +0300

    Set longitude and latitude by searchbox. Add cleanup function on the map load useEffect

--- src/App.js
+++ src/App.js
@@ -22,11 +22,31 @@ function App() {
 
     setMap(map)
 
-  }, [])
+    return () => map.remove()
+
+  }, [latitude, longitude])
 
   return (
     <div className="App">
-      <div ref={mapElement} className="map"></div>
+      <div ref={mapElement} className="map" />
+      <div className="search-bar">
+        <h1>Where to?</h1>
+        <input
+          type="text"
+          id="longitude"
+          className="longitude"
+          placeholder='Put in the longitude'
+          onChange={(e) => {setLongitude(e.target.value)}}
+        />
+
+        <input
+          type="text"
+          id="latitude"
+          className="latitude"
+          placeholder='Put in the latitude'
+          onChange={(e) => {setLatitude(e.target.value)}}
+        />
+      </div>
     </div>
   );
 }
